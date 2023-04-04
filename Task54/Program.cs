dfc487d9aa1af5c83fd44842ba9347c25f665b5a
Oli <oli.varnavska@gmail.com>
2023-04-04 19:36:41 +0800

    Fix Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void SelectionSort(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0) ; i++)
+    {
+        for (int j = 0; j < matr.GetLength(1)- 1; j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < matr.GetLength(1); k++)
+            {
+                if (matr[i, k] < matr[i, minPosition]) minPosition = k;
+            }
+            int temp = matr[i, j];
+        matr[i,j] = matr [i,minPosition];
+        matr[i, minPosition] = temp;
+        }
+
+        
+    }
+}
+
+
+int[,] matrix = new int[rows, columns];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+SelectionSort(matrix);
+PrintArray(matrix);
